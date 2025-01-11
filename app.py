54591f7326b8be5a178432eece9a7a1bb447694c
Aditya2413 <adityaugale4652@gmail.com>
2025-01-11 13:53:30 +0530

    checked data ingestion working

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_ingestion import DataIngestion, DataIngestionConfig
+import sys
+
+if __name__ == "__main__":
+    logging.info("Application started")
+    try:
+        # data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info("Error occurred")
+        raise CustomException(e,sys)
