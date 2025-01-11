b64a0118b63e8c3a5af01f78d83948d68bf0d156
Aditya2413 <adityaugale4652@gmail.com>
2025-01-11 13:51:07 +0530

    exception.py file updated

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python file : [{0}] at line number : [{1}] with error message : [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    error_message = "Error occurred in python file : [{0}] at line number : [{1}] with error message : [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
